b2e278b79084c69e39f83ac060887181ed77bfdf
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-06-26 08:43:16 +0300

    add main

---
+++ main.py
@@ -0,0 +1,15 @@
+from http.server import HTTPServer
+
+from server import hostName, serverPort, MyServer
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Stop")
